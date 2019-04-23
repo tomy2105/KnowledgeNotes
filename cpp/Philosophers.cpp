8761ba911aa0d7eb2f6ca47dfb9f179b65c6de01
tomy2105 <t.petrovic@inet.hr>
2019-04-23 15:18:12 +0200

    Update Philosophers.cpp

--- cpp/Philosophers.cpp
+++ cpp/Philosophers.cpp
@@ -6,6 +6,7 @@
 #include <map>
 #include <sstream>
 #include <condition_variable>
+#include <vector>
 
 class osyncstream : public std::stringstream
 {
@@ -27,10 +28,17 @@ public:
 		std::mutex::lock();
 		osyncstream() << "Philosopher " << User::getIdFromThread() << " took fork " << forkId << std::endl;
 	}
-	
+
 	void unlock() {
 		osyncstream() << "Philosopher " << User::getIdFromThread() << " leaving fork " << forkId << std::endl;
 		std::mutex::unlock();
+	}
+
+	bool try_lock() {
+		osyncstream() << "Philosopher " << User::getIdFromThread() << " tries taking fork " << forkId << std::endl;
+		bool res = std::mutex::try_lock();
+		osyncstream() << "Philosopher " << User::getIdFromThread() << " " << (res ? "took" : "did not take") << " fork " << forkId << std::endl;
+		return res;
 	}
 
 	unsigned getId() const
@@ -47,47 +55,81 @@ private:
 
 class Philosopher {
 public:
-	Philosopher(Fork<Philosopher>& fork1_, Fork<Philosopher>& fork2_) :fork1(fork1_), fork2(fork2_), philospherId(++cnt) {};
+	Philosopher(Fork<Philosopher>& fork1_, Fork<Philosopher>& fork2_) : philospherId(++cnt), fork1(fork1_), fork2(fork2_) {};
 
-	void eat(unsigned numBites, unsigned biteDuration) 
+	void eat(unsigned numBites, unsigned biteDuration)
 	{
-		{
-			std::lock_guard lck(staticMtx);
-			threadToIdMap[std::this_thread::get_id()] = philospherId;
-		}
+		threadPhilosopherId = philospherId;
 
 		osyncstream() << "Philosopher " << philospherId << " waiting for start" << std::endl;
 
 		{
-			std::unique_lock lck(staticMtx);
+			std::unique_lock<std::mutex> lck(staticMtx);
 			start.wait(lck);
 		}
+
+		osyncstream() << "Philosopher " << philospherId << " starting to eat" << std::endl;
+		auto t0 = std::chrono::high_resolution_clock::now();
 
 		for (unsigned biteNum = 1; biteNum <= numBites; biteNum++) {
 			{
 				// scoped_lock worries about deadlocking so we don't have to :) :) :)
+				// on the other hand there will be a some number of try_lock and locks obtained and then released (because other lock has not been obtained) when used
+				// check here for extremely nice article about that: https://howardhinnant.github.io/dining_philosophers.html
+				// scoped lock is introduced in C++17
 				std::scoped_lock lockForks(fork1, fork2);
 				osyncstream() << "Philosopher " << philospherId << " starts eating bite " << biteNum << " with fork " << fork1.getId() << " and fork " << fork2.getId() << std::endl;
 				std::this_thread::sleep_for(std::chrono::seconds(biteDuration));
-				osyncstream() << "Philosopher " << philospherId << " finishes eating" << std::endl;
+				osyncstream() << "Philosopher " << philospherId << " finishes eating bite " << biteNum << std::endl;
 			}
 		}
 
-		{
-			std::lock_guard lck(staticMtx);
-			threadToIdMap.erase(threadToIdMap.find(std::this_thread::get_id()));
-		}
+		auto t1 = std::chrono::high_resolution_clock::now();
+		osyncstream() << "Philosopher " << philospherId << " finished eating in " << std::chrono::duration<float>(t1 - t0).count() << " seconds" << std::endl;
 	}
 
-	unsigned getId() const
+	void eat_ordered(unsigned numBites, unsigned biteDuration)
 	{
-		return philospherId;
+		threadPhilosopherId = philospherId;
+
+		osyncstream() << "Philosopher " << philospherId << " waiting for start" << std::endl;
+
+		{
+			std::unique_lock<std::mutex> lck(staticMtx);
+			start.wait(lck);
+		}
+
+		osyncstream() << "Philosopher " << philospherId << " starting to eat" << std::endl;
+		auto t0 = std::chrono::high_resolution_clock::now();
+
+		for (unsigned biteNum = 1; biteNum <= numBites; biteNum++) {
+			{
+				// making sure lock acquire is ordered
+				if (fork1.native_handle() < fork2.native_handle()) {
+					fork1.lock();
+					fork2.lock();
+				}
+				else {
+					fork2.lock();
+					fork1.lock();
+				}
+				// making sure already owned locks are released at the end of the scope
+				std::lock_guard<Fork<Philosopher>> lock1(fork1, std::adopt_lock);
+				std::lock_guard<Fork<Philosopher>> lock2(fork2, std::adopt_lock);
+
+				osyncstream() << "Philosopher " << philospherId << " starts eating bite " << biteNum << " with fork " << fork1.getId() << " and fork " << fork2.getId() << std::endl;
+				std::this_thread::sleep_for(std::chrono::seconds(biteDuration));
+				osyncstream() << "Philosopher " << philospherId << " finishes eating bite " << biteNum << std::endl;
+			}
+		}
+
+		auto t1 = std::chrono::high_resolution_clock::now();
+		osyncstream() << "Philosopher " << philospherId << " finished eating in " << std::chrono::duration<float>(t1 - t0).count() << " seconds" << std::endl;
 	}
 
 	static unsigned getIdFromThread()
 	{
-		std::lock_guard lck(staticMtx);
-		return threadToIdMap.at(std::this_thread::get_id());
+		return threadPhilosopherId;
 	}
 
 	static void signalStart()
@@ -101,14 +143,16 @@ private:
 	Fork<Philosopher>& fork2;
 
 	static std::atomic<unsigned> cnt;
-	static std::map<std::thread::id, unsigned> threadToIdMap;
+	static thread_local unsigned threadPhilosopherId;
 	static std::mutex staticMtx;
 	static std::condition_variable start;
 };
 
-std::atomic<unsigned> Fork<Philosopher>::cnt = 0;
-std::atomic<unsigned> Philosopher::cnt = 0;
-std::map<std::thread::id, unsigned> Philosopher::threadToIdMap;
+template<class User> std::atomic<unsigned> Fork<User>::cnt;
+std::atomic<unsigned> Philosopher::cnt;
+
+thread_local unsigned Philosopher::threadPhilosopherId = -1;
+
 std::mutex Philosopher::staticMtx;
 std::condition_variable Philosopher::start;
 
@@ -122,9 +166,23 @@ int main()
 	std::vector<std::thread> philosopherThreadObjects;
 	std::array<Fork<Philosopher>, NUM_PHILOSOPHERS> forks;
 
-	for (auto i = 0; i < NUM_PHILOSOPHERS; i++) {
+	for (unsigned i = 0; i < NUM_PHILOSOPHERS; i++) {
 		philosophers.emplace_back(forks[i % NUM_PHILOSOPHERS], forks[(i + 1) % NUM_PHILOSOPHERS]);
 		philosopherThreadObjects.emplace_back(&Philosopher::eat, philosophers[i], NUM_BITES, BITE_DURATION);
+	}
+	std::this_thread::sleep_for(std::chrono::seconds(BITE_DURATION));
+	Philosopher::signalStart();
+
+	for (auto& th : philosopherThreadObjects) {
+		th.join();
+	}
+
+	philosopherThreadObjects.clear();
+	philosophers.clear();
+
+	for (unsigned i = 0; i < NUM_PHILOSOPHERS; i++) {
+		philosophers.emplace_back(forks[i % NUM_PHILOSOPHERS], forks[(i + 1) % NUM_PHILOSOPHERS]);
+		philosopherThreadObjects.emplace_back(&Philosopher::eat_ordered, philosophers[i], NUM_BITES, BITE_DURATION);
 	}
 	std::this_thread::sleep_for(std::chrono::seconds(BITE_DURATION));
 	Philosopher::signalStart();
